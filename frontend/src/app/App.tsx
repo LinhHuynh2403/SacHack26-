f6f84248c550853a95f47606fccb4c6fd40097c1
Linh Huynh <vanhuynh@ucdavis.edu>
2026-02-22 00:43:50 -0800

    wrap for progressive web app

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,12 +1,18 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
-import { ErrorBoundary } from "./ErrorHandling/ErrorBoundary";
 
 export default function App() {
   return (
-    <ErrorBoundary>
-      <RouterProvider router={router} />
-    </ErrorBoundary>
+    // The outer div is the "desktop" background (darker gray)
+    <div className="min-h-screen bg-gray-200 flex justify-center">
+
+      {/* The inner div is your "mobile phone" screen limit */}
+      <div className="w-full max-w-[430px] bg-[#F8FAFC] min-h-screen relative shadow-2xl overflow-x-hidden">
+
+        <RouterProvider router={router} />
+
+      </div>
+
+    </div>
   );
 }
-
